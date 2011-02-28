76d6a4d40353e2c64eb050b6cb4e4595b1f0428e
Arci <fa.arcidiacono@gmail.com>
2011-02-28 11:23:06 +0100

    Display source code

---
+++ src/Display.java
@@ -0,0 +1,118 @@
+import java.util.*;
+import java.lang.*;
+import java.math.*;
+
+public class Display {
+
+	static char digit[][][] = {
+				{{' ','_',' '},{'|',' ','|'},{'|','_','|'}}, //0
+				{{' ',' ',' '},{' ',' ','|'},{' ',' ','|'}}, //1
+				{{' ','_',' '},{' ','_','|'},{'|','_',' '}}, //2
+				{{' ','_',' '},{' ','_','|'},{' ','_','|'}}, //3
+				{{' ',' ',' '},{'|','_','|'},{' ',' ','|'}}, //4
+				{{' ','_',' '},{'|','_',' '},{' ','_','|'}}, //5
+				{{' ','_',' '},{'|','_',' '},{'|','_','|'}}, //6
+				{{' ','_',' '},{' ',' ','|'},{' ',' ','|'}}, //7
+				{{' ','_',' '},{'|','_','|'},{'|','_','|'}}, //8
+				{{' ','_',' '},{'|','_','|'},{' ','_','|'}}  //9		
+				};
+	/* to do
+	static char signes[][][] = {
+				{{' ',' ',' '},{' ',' ',' '},{' ',' ',' '}}, //0
+				{{' ',' ',' '},{' ',' ',' '},{' ',' ',' '}}, //1
+				{{' ',' ',' '},{' ',' ',' '},{' ',' ',' '}}, //2
+				{{' ',' ',' '},{' ',' ',' '},{' ',' ',' '}}  //3
+				};*/
+	
+	
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		System.out.printf("Inserisci un numero: ");
+		String number = in.nextLine();
+	    
+
+	}
+	
+	
+	public static int stringToInt(String number){
+		int n = 0;
+		for(int j=0, i=number.length()-1; i>=0; j++, i--)
+			n += Character.getNumericValue( number.charAt(i)) * Math.pow( 10,j);
+		return n;
+	}
+	
+	public static void numberToDigit(String number){
+		
+		for(int i=0; i<number.length(); i++)
+			numberToDigitFirstLine( Character.getNumericValue( number.charAt(i)));
+		System.out.printf("\n");
+		
+		for(int i=0; i<number.length(); i++)
+			numberToDigitSecondLine( Character.getNumericValue( number.charAt(i)));
+		System.out.printf("\n");
+		
+		for(int i=0; i<number.length(); i++)
+			numberToDigitThirdLine( Character.getNumericValue( number.charAt(i)));
+		System.out.printf("\n");
+
+	}
+	
+	private static void numberToDigitFirstLine(int number){
+		
+		int i=0;
+		for(int j=0; j<3; j++){
+			System.out.printf("%c",digit[number][i][j]);					
+			}
+		System.out.printf(" ");
+	}
+	
+	private static void numberToDigitSecondLine(int number){
+		
+		int i=1;
+		for(int j=0; j<3; j++){
+			System.out.printf("%c",digit[number][i][j]);					
+			}
+		System.out.printf(" ");
+	}
+	
+	private static void numberToDigitThirdLine(int number){
+		
+		int i=2;
+		for(int j=0; j<3; j++){
+			System.out.printf("%c",digit[number][i][j]);					
+			}
+		System.out.printf(" ");
+	}
+	
+	/*
+	public static void toDigitHorizontal(){
+		
+		for(int i=0; i<3; i++){
+			for(int j=0; j<10; j++){
+				for(int w=0; w<3; w++){
+					System.out.print( digit[j][i][w] );					
+				}
+				System.out.print(" ");
+			}
+			System.out.println();
+		}
+			
+	}
+	
+	public static void toDigitVertical(){
+	
+		for(int i=0; i<10; i++){
+			for(int j=0; j<3; j++){
+				for(int w=0; w<3; w++){
+					System.out.print( digit[i][j][w]);
+				}
+				System.out.println();
+			}
+		System.out.println();
+			
+		}
+	}*/
+}
+
+	
