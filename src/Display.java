51149113302ededc5d8f347b0db0d7b526c1d494
Arci <fa.arcidiacono@gmail.com>
2011-03-01 14:48:22 +0100

    Aggiunto supporto virgola mobile (deeky)

--- src/Display.java
+++ src/Display.java
@@ -4,154 +4,113 @@ import java.math.*;
 
 public class Display {
 
-	static char digit[][][] = {
-				{{' ','_',' '},{'|',' ','|'},{'|','_','|'}}, //0
-				{{' ',' ',' '},{' ',' ','|'},{' ',' ','|'}}, //1
-				{{' ','_',' '},{' ','_','|'},{'|','_',' '}}, //2
-				{{' ','_',' '},{' ','_','|'},{' ','_','|'}}, //3
-				{{' ',' ',' '},{'|','_','|'},{' ',' ','|'}}, //4
-				{{' ','_',' '},{'|','_',' '},{' ','_','|'}}, //5
-				{{' ','_',' '},{'|','_',' '},{'|','_','|'}}, //6
-				{{' ','_',' '},{' ',' ','|'},{' ',' ','|'}}, //7
-				{{' ','_',' '},{'|','_','|'},{'|','_','|'}}, //8
-				{{' ','_',' '},{'|','_','|'},{' ','_','|'}}  //9		
-				};
-	/*TODO
-	static char signes[][][] = {
-				{{' ',' ',' '},{' ',' ',' '},{' ',' ',' '}}, //0
-				{{' ',' ',' '},{' ',' ',' '},{' ',' ',' '}}, //1
-				{{' ',' ',' '},{' ',' ',' '},{' ',' ',' '}}, //2
-				{{' ',' ',' '},{' ',' ',' '},{' ',' ',' '}}  //3
-				};
-	*/
-	
-	
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-                
-		System.out.printf("Inserisci un numero: ");
-		String num1 = in.nextLine();
-		numberToDigit(num1);
-		System.out.printf("Inserisci un numero: ");
-		String num2 = in.nextLine();
-		numberToDigit(num2);
-		System.out.printf("La loro somma è: \n");
-		addDigit(num1,num2);
-		
-		/*
-		System.out.printf("to vertical \n");
-		toDigitVertical();
-		System.out.printf("to horizontal \n");
-		toDigitHorizontal();
-		*/
-	    
+    static char digit[][][] = {
+			    {{' ','_',' '},{'|',' ','|'},{'|','_','|'}}, //0
+			    {{' ',' ',' '},{' ',' ','|'},{' ',' ','|'}}, //1
+			    {{' ','_',' '},{' ','_','|'},{'|','_',' '}}, //2
+                            {{' ','_',' '},{' ','_','|'},{' ','_','|'}}, //3
+                            {{' ',' ',' '},{'|','_','|'},{' ',' ','|'}}, //4
+                            {{' ','_',' '},{'|','_',' '},{' ','_','|'}}, //5
+                            {{' ','_',' '},{'|','_',' '},{'|','_','|'}}, //6
+                            {{' ','_',' '},{' ',' ','|'},{' ',' ','|'}}, //7
+                            {{' ','_',' '},{'|','_','|'},{'|','_','|'}}, //8
+                            {{' ','_',' '},{'|','_','|'},{' ','_','|'}}  //9		
+                            };
+    
+    static char comma[][] = {{' ',' ','.'}};
+    
+    /* to do
+    static char signes[][][] = {
+                            {{' ',' ',' '},{' ',' ',' '},{' ',' ',' '}}, //0
+                            {{' ',' ',' '},{' ',' ',' '},{' ',' ',' '}}, //1
+                            {{' ',' ',' '},{' ',' ',' '},{' ',' ',' '}}, //2
+                            {{' ',' ',' '},{' ',' ',' '},{' ',' ',' '}}  //3
+                            };
+    */
+    
+    
+    public static void main(String[] args) {
+            
+        Scanner in = new Scanner(System.in);
+        
+        System.out.printf("Inserisci un numero: ");
+        String number = in.nextLine();
+        
+        if(isDouble(number))
+             doubleToDigit(number);
+        else
+             intToDigit(number);
+    }
+    
+    
+    public static boolean isDouble(String number){
+            
+        return Double.parseDouble(number) - Math.rint(Double.parseDouble(number)) != 0;
+                    
+    }
+    
+    public static void doubleToDigit(String number){
+        
+        for(int i=0; i<number.length(); i++)
+                if(number.charAt(i) != '.')        
+                        numberToDigitFirstLine( Character.getNumericValue( number.charAt(i)));
+                else
+                        System.out.printf("%c",comma[0][0]);
+        System.out.printf("\n");
+        
+        for(int i=0; i<number.length(); i++)
+                if(number.charAt(i) != '.')
+                        numberToDigitSecondLine( Character.getNumericValue( number.charAt(i)));
+                else
+                        System.out.printf("%c",comma[0][1]);
+        System.out.printf("\n");
+        
+        for(int i=0; i<number.length(); i++)
+                if(number.charAt(i) != '.')
+                        numberToDigitThirdLine( Character.getNumericValue( number.charAt(i)));
+                else
+                        System.out.printf("%c",comma[0][2]);
+        System.out.printf("\n");               
+    }
+    
+    public static void intToDigit(String number){
+            
+        for(int i=0; i<number.length(); i++)
+                numberToDigitFirstLine( Character.getNumericValue( number.charAt(i)));
+        System.out.printf("\n");
+        
+        for(int i=0; i<number.length(); i++)
+                numberToDigitSecondLine( Character.getNumericValue( number.charAt(i)));
+        System.out.printf("\n");
+        
+        for(int i=0; i<number.length(); i++)
+                numberToDigitThirdLine( Character.getNumericValue( number.charAt(i)));
+        System.out.printf("\n");
 
-	}
-	
-	/*
-	 * add and print to digit 
-	 * @param: the number to convert
-	 */
-	public static void addDigit(String num1, String num2){
-		int a,b,c;
-		a = stringToInt(num1);
-		b= stringToInt(num2);
-		c = a+b;
-		numberToDigit(String.valueOf(c));
-	}
-	
-	/*
-	 * convert a string into int
-	 * @param: the string to convert
-	 * @return: the converted number
-	 */
-	public static int stringToInt(String number){
-		int n = 0;
-		for(int j=0, i=number.length()-1; i>=0; j++, i--)
-			n += Character.getNumericValue( number.charAt(i)) * Math.pow( 10,j);
-		return n;
-	}
-	
-	/*
-	 * print the number passed like 7-segment display 
-	 * @param: the number to convert
-	 */
-	public static void numberToDigit(String number){
-		
-		for(int i=0; i<number.length(); i++)
-			numberToDigitFirstLine( Character.getNumericValue( number.charAt(i)));
-		System.out.printf("\n");
-		
-		for(int i=0; i<number.length(); i++)
-			numberToDigitSecondLine( Character.getNumericValue( number.charAt(i)));
-		System.out.printf("\n");
-		
-		for(int i=0; i<number.length(); i++)
-			numberToDigitThirdLine( Character.getNumericValue( number.charAt(i)));
-		System.out.printf("\n");
-
-	}
-	
-	private static void numberToDigitFirstLine(int number){
-		
-		int i=0;
-		for(int j=0; j<3; j++){
-			System.out.printf("%c",digit[number][i][j]);					
-			}
-		System.out.printf(" ");
-	}
-	
-	private static void numberToDigitSecondLine(int number){
-		
-		int i=1;
-		for(int j=0; j<3; j++){
-			System.out.printf("%c",digit[number][i][j]);					
-			}
-		System.out.printf(" ");
-	}
-	
-	private static void numberToDigitThirdLine(int number){
-		
-		int i=2;
-		for(int j=0; j<3; j++){
-			System.out.printf("%c",digit[number][i][j]);					
-			}
-		System.out.printf(" ");
-	}
-	
-	/*
-	 *Print digit from 0 to 9 in horizontal
-	 */
-	public static void toDigitHorizontal(){
-		
-		for(int i=0; i<3; i++){
-			for(int j=0; j<10; j++){
-				for(int w=0; w<3; w++){
-					System.out.print( digit[j][i][w] );					
-				}
-				System.out.print(" ");
-			}
-			System.out.println();
-		}
-			
-	}
-	
-	/*
-	 *Print digit from 0 to 9 in vertical
-	 */
-	public static void toDigitVertical(){
-	
-		for(int i=0; i<10; i++){
-			for(int j=0; j<3; j++){
-				for(int w=0; w<3; w++){
-					System.out.print( digit[i][j][w]);
-				}
-				System.out.println();
-			}
-		System.out.println();
-			
-		}
-	}
+    }
+    
+    private static void numberToDigitFirstLine(int number){
+            
+        for(int j=0; j<3; j++){
+                System.out.printf("%c",digit[number][0][j]);					
+                }
+        System.out.printf(" ");
+    }
+    
+    private static void numberToDigitSecondLine(int number){
+            
+        for(int j=0; j<3; j++){
+                System.out.printf("%c",digit[number][1][j]);					
+                }
+        System.out.printf(" ");
+    }
+    
+    private static void numberToDigitThirdLine(int number){
+        
+        for(int j=0; j<3; j++){
+                System.out.printf("%c",digit[number][2][j]);					
+                }
+        System.out.printf(" ");
+    }
+     
 }
-
-	
